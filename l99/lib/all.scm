1777b4adb969905874a9c0b421e23a8ac31abff6
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-08-21 16:55:51 +0200

    add problems as comments

--- l99/lib/all.scm
+++ l99/lib/all.scm
@@ -1,18 +1,32 @@
+;(*) Find the last box of a list.
+;    Example:
+;    * (my-last '(a b c d))
+;    (D)
 (define (my-last ls)
   (if (null? (cdr ls))
       ls
       (my-last (cdr ls))))
 
+;(*) Find the last but one box of a list.
+;    Example:
+;    * (my-but-last '(a b c d))
+;    (C D)
 (define (my-but-last ls)
   (if (null? (cddr ls))
       ls
       (my-last (cdr ls))))
 
+;(*) Find the K'th element of a list.
+;    The first element in the list is number 1.
+;    Example:
+;    * (element-at '(a b c d e) 3)
+;    C
 (define (element-at ls n)
   (if (zero? (- n 1))
       (car ls)
       (element-at (cdr ls) (- n 1))))
 
+;(*) Find the number of elements of a list.
 (define (my-length ls)
   (if (null? ls)
       0
