6d666bfec5acbb01ea4072e89bd5046bdde7b6ab
Tanguy Andreani <tanguy.andreani@tuta.io>
2020-08-21 16:53:07 +0200

    remove useless type verifications

--- l99/lib/all.scm
+++ l99/lib/all.scm
@@ -1,23 +1,17 @@
 (define (my-last ls)
-  (if (or (not (list? ls)) (null? ls))
-      'undefined
-      (if (null? (cdr ls))
-          ls
-          (my-last (cdr ls)))))
+  (if (null? (cdr ls))
+      ls
+      (my-last (cdr ls))))
 
 (define (my-but-last ls)
-  (if (or (not (list? ls)) (null? ls) (null? (cdr ls)))
-      'undefined
-      (if (null? (cddr ls))
-          ls
-          (my-last (cdr ls)))))
+  (if (null? (cddr ls))
+      ls
+      (my-last (cdr ls))))
 
 (define (element-at ls n)
-  (if (or (not (list? ls)) (null? ls))
-      'undefined
-      (if (zero? (- n 1))
-          (car ls)
-          (element-at (cdr ls) (- n 1)))))
+  (if (zero? (- n 1))
+      (car ls)
+      (element-at (cdr ls) (- n 1))))
 
 (define (my-length ls)
   (if (null? ls)
--- l99/test_all.scm
+++ l99/test_all.scm
@@ -29,12 +29,6 @@
 (assert 'my-last-single-element
   (equal? (my-last '(1))
           '(1)))
-(assert 'my-last-empty-list
-  (equal? (my-last '())
-          'undefined))
-(assert 'my-last-not-a-list
-  (equal? (my-last 'hello)
-          'undefined))
 
 
 (assert 'my-but-last-normal-list
@@ -43,15 +37,6 @@
 (assert 'my-but-last-single-element
   (equal? (my-but-last '(1 2))
           '(1 2)))
-(assert 'my-but-last-single-element
-  (equal? (my-but-last '(1))
-          'undefined))
-(assert 'my-but-last-empty-list
-  (equal? (my-but-last '())
-          'undefined))
-(assert 'my-but-last-not-a-list
-  (equal? (my-but-last 'hello)
-          'undefined))
 
 (assert 'element-at-element-exit
   (equal? (element-at '(4 3 2 1) 3)
